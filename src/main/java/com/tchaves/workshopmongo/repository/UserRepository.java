3b2f2ace5a4da3295330453d5692e87cb9469351
tchaves <tchaves@a5solutions.com>
2019-05-16 14:59:03 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/tchaves/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tchaves.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tchaves.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
